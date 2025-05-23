170f3229dfd84d7ae28f5d29f65f17f45ac8e3c9
Aditi Raghuwanshi <aditi.raghuwanshi430@nmims.edu.in>
2025-05-22 19:11:47 -0700

    url issue

--- src/utils/axiosinstance.js
+++ src/utils/axiosinstance.js
@@ -1,31 +1,73 @@
 
 
 
+// import axios from "axios";
+// import { BASE_URL } from "./constant";
+
+// const axiosInstance = axios.create({
+
+
+//     	VITE_SERVER_URL: BASE_URL,
+//     // timeout: 10000,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// });
+// console.log("BASE_URL is", BASE_URL)
+
+// axiosInstance.interceptors.request.use(
+//     (config) => {
+//         const accessToken = localStorage.getItem("token");
+//         if(accessToken) {
+//             config.headers.Authorization = `Bearer ${accessToken}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
+
+// export default axiosInstance;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import axios from "axios";
-import { BASE_URL } from "./constant";
 
+export const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
+console.log("Base URL is", BASE_URL); // ✅ for debugging
 
 const axiosInstance = axios.create({
-    	VITE_SERVER_URL: BASE_URL,
-    // timeout: 10000,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-console.log("BASE_URL is", BASE_URL)
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const accessToken = localStorage.getItem("token");
-        if(accessToken) {
-            config.headers.Authorization = `Bearer ${accessToken}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
