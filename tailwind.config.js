b0ec52279d7413095d63c4122b12cdb3895f8f24
Aditi Raghuwanshi <aditi.raghuwanshi430@nmims.edu.in>
2025-05-23 15:10:22 -0700

    env issues

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandBlue: '#1e40af',
+        brandPink: '#db2777',
+      },
+    },
+  },
+  plugins: [],
+}
